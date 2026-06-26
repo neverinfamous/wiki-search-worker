3102565f493ee1e402b2d5a5ea2472710a2c4e10
Chris <admin@adamic.tech>
2026-06-26 13:03:57 -0400

    fix(csp): add manifest-src to allow webmanifest from main domain
    History-Category: Changed
    History-Impact: 0.1
    History-Confidence: 1
    History-Validation: none

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -12,7 +12,7 @@ export const MAX_LOG_QUERY_LENGTH = 30;
 export const TURNSTILE_VERIFY_URL = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
 export const DEFAULT_MATCH_THRESHOLD = 0.5;
 export const DEFAULT_INSTANCE_ID = 'adamic-blog-search';
-export const HTML_CSP = "default-src 'self'; script-src 'self' 'unsafe-inline' https://challenges.cloudflare.com https://static.cloudflareinsights.com; frame-src https://challenges.cloudflare.com; style-src 'self' 'unsafe-inline' https://adamic.tech; img-src 'self' data: https://adamic.tech; connect-src 'self' https://cloudflareinsights.com";
+export const HTML_CSP = "default-src 'self'; script-src 'self' 'unsafe-inline' https://challenges.cloudflare.com https://static.cloudflareinsights.com; frame-src https://challenges.cloudflare.com; style-src 'self' 'unsafe-inline' https://adamic.tech; img-src 'self' data: https://adamic.tech; connect-src 'self' https://cloudflareinsights.com; manifest-src 'self' https://adamic.tech";
 export const ICON_PATHS: Set<string> = new Set([
     '/favicon.ico',
     '/apple-touch-icon.png',
